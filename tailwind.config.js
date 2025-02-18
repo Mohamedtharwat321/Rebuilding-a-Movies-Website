62e73751716ad2999d4b92ea4c1984e045a3c687
Mohamedtharwat321 <mohamedtharwat313@gmail.com>
2025-02-18 02:42:05 +0200

    first

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",       // Include your main HTML file
+    "./src/**/*.{html,js}", // Include all HTML/JS files in the src folder
+  ],
+  theme: {
+    extend: {
+      colors: {
+        p1: "#ff2c1f",
+        p2: "#020307",
+        p3: "#fff",
+      },
+      gridTemplateColumns: {
+        moviesgrid: "repeat(auto-fit, minmax(180px, auto))",
+      },
+      backgroundImage: {
+        headerImage: "url(/img/home3.jpg)",
+      },
+      fontFamily: {
+        Pfont: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
